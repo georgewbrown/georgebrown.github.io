97a899d1c1e82beede13984de1982cda71e99e99
georgewbrown <396gbrown@gmail.com>
2019-06-05 13:18:14 -0400

    page transition fucntionality and general clean up

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('#fullpage').fullpage({
+		easingcss3: `cubic-bezier(0.175, 0.5, 0.5, 1)`,
+		loopTop: false,
+		loopBottom: true,
+		navigation: true,
+		navigationPosition: 'right'
+	});
+});
