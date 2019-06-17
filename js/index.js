cd53449de6cb6f90bef66ade30dafd1f17aa346b
georgewbrown <396gbrown@gmail.com>
2019-06-17 14:30:11 -0400

    renamed functions, classes, and ids

--- js/index.js
+++ js/index.js
@@ -1,35 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	$('#fullpage').fullpage({
 		easingcss3: `cubic-bezier(0.175, 0.5, 0.5, 1)`,
 		loopTop: false,
 		loopBottom: true,
-		navigation:true,
+		navigation: true,
 		navigationPosition: 'right',
 		keyboardScrolling: true,
 		controlArrows: true
 	});
 });
 
-document.querySelector(".about-button").addEventListener('click', addStyling);
+document.querySelector(".about-button").addEventListener('click', addOverlay);
 
+let overlay = document.getElementById("overlay-insert");
 
-function addStyling() {
-	let	darkenDiv = document.createElement('div');
-	let buttonContainer = document.getElementsByClassName('button-container');
-
-	
-	darkenDiv.classList = "darken-div";
-	divContainer[0].innerHTML = "Hello World!";
-	console.log(buttonContainer);
-
-	buttonContainer[0].style.display = "none";
-	document.body.appendChild(darkenDiv);
+function addOverlay() {
+	// TODO: Make local var global
+	let navBar = document.getElementById('nav-bar');
+	navBar.classList.add("disabled");
+	overlay.style.width = "100%";
 }
 
-function openNav() {
-	document.getElementById("overlay-insert").style.width = "100%";
-  }
 
-  function closeNav() {
-	document.getElementById("overlay-insert").style.width = "0%";
-  }
+function removeOverlay() {
+	// TODO: Make local var global
+	let navBar = document.getElementById('nav-bar');
+	navBar.classList.remove("disabled");
+	overlay.style.width = "0%";
+}
