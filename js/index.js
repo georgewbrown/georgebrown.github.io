9863d2c31c7b8bb1af9a40738232e01652297076
georgewbrown <396gbrown@gmail.com>
2019-06-19 21:20:25 -0400

    [WIP] modulize overlay functionality

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 	$('#fullpage').fullpage({
 		licenseKey: '',
-		anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+		// anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
 		easingcss3: `cubic-bezier(0.175, 0.5, 0.5, 1)`,
 		loopTop: false,
 		loopBottom: true,
@@ -12,62 +12,42 @@ $(document).ready(function () {
 	});
 });
 
-let overlay = document.getElementById("overlay-insert");
-let projectText = document.getElementsByClassName("project-description");
-// let about = document.querySelectorAll(".about-button").addEventListener('click', addOverlay);
+const overlay = document.getElementById("overlay-insert");
+const projectText = document.getElementsByClassName("project-description");
 
-
-function projectTextOpactiySwitch() {
-	for (i = 0; i < projectText.length; i++) { 
-		if (projectText[i].style.opacity === 1) {
-			projectText[i].style.opacity = 1;
-		} else if (projectText[i].style.opacity === 1) {
-			projectText[i].style.opacity = 0;
-		} else {
-			projectText[i].style.opacity = 0;
-		}
-	}
-	console.log(projectText[0].style.opacity);
-}
-
-console.log(projectText[0].style.opacity);
 function addOverlay() {
-	// TODO: Make local vars global
-	let navBar = document.getElementById('nav-bar'); // grabs Navigation Bar element
-	let dotScroll = document.getElementById('fp-nav'); // grabs Dot Scroll Bar element
+
+	const navBar = document.getElementById('nav-bar'); // grabs Navigation Bar element
+	const dotScroll = document.getElementById('fp-nav'); // grabs Dot Scroll Bar element
 
 	dotScroll.classList.add("disabled"); // disables Dot Scroll Bar
 	navBar.classList.add("disabled"); //disables Navigation Bar
 	overlay.style.width = "100%";
 
-	projectTextOpactiySwitch();
-	// for (i = 0; i < projectText.length; i++) { 
-	// 	if (projectText[i].style.opacity === 1) {
-	// 		projectText[i].style.opacity = 0;
-	// 	} else {
-	// 		projectText[i].style.opacity = 1;
-	// 	}
-	// }
+	for (i = 0; i < projectText.length; i++) {
+		if (projectText[i].style.opacity === 1) {
+			projectText[i].style.opacity = 0;
+		} else {
+			projectText[i].style.opacity = 1;
+		}
+	}
 }
 
 function removeOverlay() {
-	// TODO: Make local vars global
-	let navBar = document.getElementById('nav-bar'); // grabs Navigation Bar element
-	let dotScroll = document.getElementById('fp-nav'); // Dot Scroll Bar Bar element
+
+	const navBar = document.getElementById('nav-bar'); // grabs Navigation Bar element
+	const dotScroll = document.getElementById('fp-nav'); // Dot Scroll Bar Bar element
 
 	navBar.classList.remove("disabled"); // renables Dot Scroll Bar
 	dotScroll.classList.remove("disabled"); // renables Navigation Bar
 	overlay.style.width = "0%";
-	projectTextOpactiySwitch();
 
-	// for (i = 0; i < projectText.length; i++) {
-
-	// 	if (projectText[i].style.opacity === 0) {
-	// 		projectText[i].style.opacity = 1;
-
-	// 	} else {
-	// 		projectText[i].style.opacity = 0;
-	// 	}
-	// }
+	for (i = 0; i < projectText.length; i++) {
+		if (projectText[i].style.opacity === 0) {
+			projectText[i].style.opacity = 1;
+		} else {
+			projectText[i].style.opacity = 0;
+		}
+	}
 
 }
