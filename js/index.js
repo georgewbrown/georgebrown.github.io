169d69f868200e4231f129cbbf255d725edcb79f
georgewbrown <396gbrown@gmail.com>
2019-06-20 20:57:10 -0400

    [WIP] Fixing Bug that lets for loop repeat first loop twice

--- js/index.js
+++ js/index.js
@@ -12,8 +12,8 @@ $(document).ready(function () {
 	});
 });
 
-const overlay = document.getElementById("overlay-insert");
 const projectText = document.getElementsByClassName("project-description");
+const overlay = document.getElementsByClassName("overlay-insert");
 
 function addOverlay() {
 
@@ -22,7 +22,34 @@ function addOverlay() {
 
 	dotScroll.classList.add("disabled"); // disables Dot Scroll Bar
 	navBar.classList.add("disabled"); //disables Navigation Bar
-	overlay.style.width = "100%";
+	for ( var j = 0, len = overlay.length; j < len; j++ ) {
+		if (j === 0) {
+		overlay[j].style.width = "100%";
+		}else if (j === 1) {
+		overlay[j].style.width = "100%";
+		} else {
+		overlay[j].style.width = "100%";
+		}
+		// switch (o) {
+			// 	case 1:
+			// 	overlay[1].style.width = "100%";
+			// 	break;
+			// 	case 2:
+			// 	overlay[2].style.width = "100%";
+			// 	break;
+			// 	case 3:
+			// 	overlay[3].style.width = "100%";
+			// 	break;
+			// 	case 4:
+			// 	overlay[4].style.width = "100%";
+			// 	break;
+			// 	default:
+			// 	overlay[0].style.width = "100%";
+			// }
+			console.log(j);
+
+		}
+		console.log(overlay, j);
 
 	for (i = 0; i < projectText.length; i++) {
 		if (projectText[i].style.opacity === 1) {
@@ -40,7 +67,24 @@ function removeOverlay() {
 
 	navBar.classList.remove("disabled"); // renables Dot Scroll Bar
 	dotScroll.classList.remove("disabled"); // renables Navigation Bar
-	overlay.style.width = "0%";
+
+		switch (o){
+			case 1:
+			overlay[1].style.width = "0%";
+			break;
+			case 2:
+			overlay[2].style.width = "0%";
+			break;
+			case 3:
+			overlay[3].style.width = "0%";
+			break;
+			case 4:
+			overlay[4].style.width = "0%";
+			break;
+			default:
+			overlay[0].style.width = "0%";
+		}
+	
 
 	for (i = 0; i < projectText.length; i++) {
 		if (projectText[i].style.opacity === 0) {
