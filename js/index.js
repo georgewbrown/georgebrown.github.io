3d60dd6dbaba8fe7f733e40b8e605a95169f6100
georgewbrown <396gbrown@gmail.com>
2019-07-11 16:03:34 -0400

    added github and linkedin icons

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 		navigationPosition: 'right',
 		keyboardScrolling: true,
 		controlArrows: true,
-		onLeave: () => { // janky solution
+		onLeave: () => {
 			for (i = 0; i < overlay.length; i++) {
 				overlay[i].style.width = "0%";
 				projectText[i].style.opacity = 0;
@@ -18,18 +18,18 @@ $(document).ready(function () {
 	});
 });
 
-// const aqText = document.getElementById('aq-project-description');
 const overlay = document.querySelectorAll(".overlay-insert");
 const projectText = document.getElementsByClassName("project-description");
 
 function addOverlay() {
 
 	const navBar = document.getElementById('nav-bar'); // grabs Navigation Bar element
+	const bottomBar = document.getElementById('bottom-bar'); // grabs Navigation Bar element
 	const dotScroll = document.getElementById('fp-nav'); // grabs Dot Scroll Bar element
 
-	// aqText.style.display ='block';
 	dotScroll.classList.add("disabled"); // disables Dot Scroll Bar
 	navBar.classList.add("disabled"); //disables Navigation Bar
+	bottomBar.classList.add("disabled"); //disables Bottom Bar
 
 	for (i = 0; i < overlay.length; i++) {
 		overlay[i].style.width = "100%";
@@ -46,11 +46,12 @@ function addOverlay() {
 function removeOverlay() {
 
 	const navBar = document.getElementById('nav-bar'); // grabs Navigation Bar element
+	const bottomBar = document.getElementById('bottom-bar'); // grabs Navigation Bar element
 	const dotScroll = document.getElementById('fp-nav'); // Dot Scroll Bar Bar element
-
-	// aqText.style.display ='none';
+	
 	navBar.classList.remove("disabled"); // renables Dot Scroll Bar
 	dotScroll.classList.remove("disabled"); // renables Navigation Bar
+	bottomBar.classList.remove("disabled"); //disables Bottom Bar
 
 	for (i = 0; i < overlay.length; i++) {
 		overlay[i].style.width = "0%";
@@ -66,14 +67,3 @@ function removeOverlay() {
 	}
 
 }
-
-// 	BUG RETURNS LOOPS TWICE THROUGH ARRAY
-// 	for ( j = 0; j < overlay.length; j++ ) {
-// 		if (overlay[j] === overlay[0] && innerHtml.includes('fp-viewing-2')) {
-// 		overlay[0].style.width = "100%";
-// 		}else if (overlay[j] === overlay[1] && innerHtml.includes('fp-viewing-4')) {
-// 		overlay[1].style.width = "100%";
-// 		} else {
-// 		console.log('it broke');
-// }
-// }
