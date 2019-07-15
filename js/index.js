e91e2070c48871eced85e60f8303bd742a218175
georgewbrown <396gbrown@gmail.com>
2019-07-15 01:27:43 -0400

    Make New Design Responsive for larger displays

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
 		controlArrows: false,
 		slidesNavigation:false,
 		scrollHorizontally: true,
+		responsiveSlides: true,
 		anchors:['home'],
 		onLeave: () => {
 			for (i = 0; i < overlay.length; i++) {
