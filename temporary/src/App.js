ba898b93358b9ad0939f5fcddae11fba735ffb2d
George Brown <396gbrown@gmail.com>
2021-03-23 13:51:15 -0400

    add temp

---
+++ temporary/src/App.js
@@ -0,0 +1,20 @@
+import logo from './Path1.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        🚧 Currently Under Construction 🚧
+        </p>
+        <p>
+          Check out my work on <a className="App-link" href="https://dribbble.com/georgewbrown" target="_blank" rel="noopener noreferrer"> Dribble</a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
